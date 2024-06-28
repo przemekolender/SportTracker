b10be600a125a51a851d6d967caa40f047a55502
przemekolender <olenderprzemek@gmail.com>
2024-06-28 23:48:56 +0200

    add calplot

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,7 +1,15 @@
+import sys
+sys.path.append("..")
+
 import streamlit as st
 import pandas as pd
 import altair as alt
 #import plotly.express as px
+from matplotlib.colors import LinearSegmentedColormap
+import calplot
+from getData import get_data
+
+
 
 st.set_page_config(
     page_title="US Population Dashboard",
@@ -21,3 +29,12 @@ with st.sidebar:
 
     color_theme_list = ['blues', 'cividis', 'greens', 'inferno', 'magma', 'plasma', 'reds', 'rainbow', 'turbo', 'viridis']
     selected_color_theme = st.selectbox('Select a color theme', color_theme_list)
+
+df = get_data('Treningi 2024', 0)
+df['Date'] =  pd.to_datetime(df['data'], format='%d.%m.%Y')
+df = df.set_index('Date')
+df['YN'] = (df['sport'] != '').astype(float)
+df['YN'] = df['YN'] * 2 - 1
+calplot.calplot(df['YN'], cmap="BuGn", colorbar=False)
+
+st.image
