1ad386b0415287114f682364caefde91a5b52867
przemekolender <olenderprzemek@gmail.com>
2024-07-08 21:51:51 +0200

    clean up old code

--- data/data_processing.py
+++ data/data_processing.py
@@ -5,9 +5,9 @@ import re
 import numpy as np
 
 
-###########################################################
+###############################################################################################
 # load data from Google sheet
-###########################################################
+###############################################################################################
 def get_data(sheet_name, sheet_id):
     # define the scope
     scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
@@ -29,12 +29,9 @@ def get_data(sheet_name, sheet_id):
     return records_df
 
 
-
-
-
-###########################################################
+###############################################################################################
 # fill empty rows with index, date and training name
-###########################################################
+###############################################################################################
 def fill_data(df, column_name):
     n = df.shape[0]
     index = str(df.loc[0, column_name]).replace(' ', '')
@@ -47,9 +44,9 @@ def fill_data(df, column_name):
     return df
 
 
-###########################################################
+###############################################################################################
 # filter by date using start and end
-###########################################################
+###############################################################################################
 def filter_by_period(df, column_name, start_date = '0001-01-01', end_date = '9999-12-31'):
     df[column_name] = pd.to_datetime(df[column_name])
     return df[(df[column_name] >= start_date) & (df[column_name] <= end_date)]
@@ -65,90 +62,3 @@ def help_most_reps(arr):
         for rep_str in reps_str:
             reps.append(int(rep_str.replace('x', '')))
     return max(reps)
-
-
-###########################################################
-# calculate number of kilometers runned
-###########################################################
-def sum_run_distance(df, start_date, end_date):
-    df = filter_by_period(df, start_date, end_date)
-    s = df[(df['sport'] == 'bieganie') & (df['exercise'] == 'dystans')]['details'] \
-        .apply(lambda x : str(x).replace('km', '')) \
-        .apply(lambda x : str(x).replace(' ', '')) \
-        .astype(float) \
-        .sum()
-    
-    return float(s)
-
-
-###########################################################
-# calculate running time
-###########################################################
-def sum_run_time(df, start_date, end_date):
-    df = filter_by_period(df, start_date, end_date)
-    df_ = df[(df['sport'] == 'bieganie') & (df['exercise'] == 'czas')].reset_index(drop = True)
-    df_['h'] = df_['details'].apply(lambda x : x[0:2]).astype(int)
-    df_['m'] = df_['details'].apply(lambda x : x[3:5]).astype(int)
-    df_['s'] = df_['details'].apply(lambda x : x[6:8]).astype(int)
-    df_['total_s'] = df_['h'] * 3600 + df_['m'] * 60 + df_['s']
-    s = df_['total_s'].sum()
-    
-    return int(s // 3600), int((s % 3600) // 60), int(s % 60)
-
-
-
-def find_sum_reps(arr):
-    arr_int = []
-    for i in arr:
-        arr_int.append(int(str(i).replace('x', '')))
-    return sum(arr_int)
-
-
-def find_sum_reps_concat(arr):
-    arr_int = []
-    for i in arr:
-        arr_str = i.split('x')
-        reps = int(arr_str[0]) * int(arr_str[1])
-        arr_int.append(reps)
-    return sum(arr_int)
-
-
-def sum_reps(df, start_date, end_date):
-    df = filter_by_period(df, start_date, end_date)
-    df['reps1'] = df['details'] \
-        .apply(lambda x : re.findall(r'x[\d]+', str(x))) \
-        .apply(lambda x : find_sum_reps(x))
-    
-    df['reps2'] = df['details'] \
-        .apply(lambda x : re.findall(r'[\d]+x[\d]+', str(x))) \
-        .apply(lambda x : find_sum_reps_concat(x))
-    
-    df['final_reps'] = np.where(df['reps2'] == 0, df['reps1'], df['reps2'])
-
-    ex = get_data('Treningi 2024', 2)
-
-    df_ = pd.merge(
-        left=df,
-        right=ex,
-        left_on='exercise',
-        right_on = 'ćwiczenie',
-        how = 'inner'
-    )
-    df_['final_reps'] = np.where(df_['opis'] == 'na czas', 0, df_['final_reps'])
-    df_['final_reps'] = np.where(df_['sport_x'] == 'bieganie', 0, df_['final_reps'])
-
-    return df_, int(df_['final_reps'].sum())
-    
-
-def sum_kilos(sets):
-    res = 0
-    for w in sets:
-        weights = re.findall(r'[\d]+kg', w)
-        if len(weights) == 1:
-            weigth = int(weights[0].replace('kg', ''))
-            #reps = find_sum_reps(re.findall(r'x[\d]+', str(w)))
-            reps2 = find_sum_reps_concat(re.findall(r'[\d]+x[\d]+', str(w)))
-
-            res = res + weigth * reps2
-
-    return res
