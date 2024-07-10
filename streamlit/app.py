5bf2990c82d33ec822d3d15e34cd94b1121eb113
przemekolender <olenderprzemek@gmail.com>
2024-07-10 23:23:29 +0200

    Add sidebar with date choice

--- streamlit/app.py
+++ streamlit/app.py
@@ -7,10 +7,12 @@ import altair as alt
 #import plotly.express as px
 from matplotlib.colors import LinearSegmentedColormap
 import calplot
+import datetime
 
-from data_processing import run_distance, run_time, reps_sum, kilos_sum, best_weight, most_reps
+from data_processing import run_distance, run_time, reps_sum, kilos_sum, best_weight, most_reps, create_date_dim, filter_by_period
 
-workouts = pd.read_csv("workouts.csv")
+
+
 
 st.set_page_config(
     page_title="SportTracker",
@@ -21,14 +23,46 @@ st.set_page_config(
 
 alt.themes.enable("dark")
 
+if "workouts" not in st.session_state:
+    st.session_state["workouts"] = pd.read_csv("workouts.csv")
+
+if "date_dim" not in st.session_state:
+    st.session_state["date_dim"] = create_date_dim(st.session_state["workouts"]['date'])
+
+if "min_date" not in st.session_state:
+    st.session_state["min_date"] = st.session_state["date_dim"]['date'].min()
+
+if "max_date" not in st.session_state:
+    st.session_state["max_date"] = st.session_state["date_dim"]['date'].max()
+
 with st.sidebar:
     st.title('🏋️‍♀️ SportTracker')
     
-    year_list = list(['2023', '2024'])[::-1]
-    
-    selected_year = st.selectbox('Select a year', year_list, index=len(year_list)-1)
+    year_list = list(st.session_state["date_dim"]['year'].unique())[::-1]
+    selected_year = st.selectbox('Wybierz rok', ['-'] + year_list, index=len(year_list)-1)
 
-    
+    month_list = list(st.session_state["date_dim"][st.session_state["date_dim"]['year'] == selected_year]['month_name'].unique())
+    selected_month = st.selectbox('Wybierz miesiąc', ['-'] + month_list)
+
+    if selected_year == '-' and selected_month == '-':
+        st.session_state["min_date"] = st.session_state["date_dim"]['date'].min()
+        st.session_state["max_date"] = st.session_state["date_dim"]['date'].max()
+
+    elif selected_year != '-' and selected_month == '-':
+        st.session_state["min_date"] = f"{selected_year}-01-01"
+        st.session_state["max_date"] = f"{selected_year}-12-31"
+
+    else:
+        selected_month_num = st.session_state["date_dim"][st.session_state["date_dim"]["month_name"] ==  selected_month]['month_str'].unique()[0]
+        min_day = '01'
+        max_day = st.session_state["date_dim"][st.session_state["date_dim"]["month_name"] ==  selected_month]['day_num'].unique()[0]
+
+        st.session_state["min_date"] = f"{selected_year}-{selected_month_num}-{min_day}"
+        st.session_state["max_date"] = f"{selected_year}-{selected_month_num}-{max_day}"
+
+
+
+workouts = filter_by_period(st.session_state["workouts"], 'date', st.session_state["min_date"], st.session_state["max_date"])
 
 distnace = run_distance(workouts)
 st.metric(label="Total run distnace", value=f"{distnace} km")
