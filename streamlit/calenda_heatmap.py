f027ec761966707a77333f5ccd28083460ec6171
przemekolender <olenderprzemek@gmail.com>
2024-07-12 00:04:06 +0200

    Add first line plot

--- streamlit/calenda_heatmap.py
+++ streamlit/calenda_heatmap.py
@@ -1,5 +1,13 @@
 import pandas as pd
 import calplot
+import plotly.express as px
 
 def calendar_heatmap(data):
     pass
+
+def month_workout_number(data):
+    fig = px.line(data, x = "month", y = "counts", line_shape='spline', markers=True)
+    fig.update_layout(
+        plot_bgcolor='white'
+    )
+    return fig
