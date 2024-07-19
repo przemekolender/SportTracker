10a78694d027c6abf9ed1e5ad8ad03f2b3f528f4
przemekolender <olenderprzemek@gmail.com>
2024-07-19 08:38:11 +0200

    Add new cards to dashboard

---
+++ streamlit/pages/1_Podsumowanie.py
@@ -0,0 +1,13 @@
+# plots with all sports summed up together
+
+import streamlit as st
+import numpy as np
+
+st.set_page_config(
+    page_title="Podsumowanie", 
+    page_icon="🏋️‍♀️", 
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+st.markdown("# Podsumowanie wszystkich treningów")
---
+++ streamlit/pages/2_Bieganie.py
@@ -0,0 +1,12 @@
+# running
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="Bieganie", 
+    page_icon="🏋️‍♀️", 
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+st.markdown("# Bieganie")
