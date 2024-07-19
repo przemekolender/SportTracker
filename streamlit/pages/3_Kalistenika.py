e7d21264c2a3c834870847631e72e476b99df639
przemekolender <olenderprzemek@gmail.com>
2024-07-19 08:38:11 +0200

    Add new cards to dashboard

---
+++ streamlit/pages/3_Kalistenika.py
@@ -0,0 +1,12 @@
+# calistenics
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="Kalistenika", 
+    page_icon="🏋️‍♀️", 
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+st.markdown("# Treningi z masą ciała")
---
+++ streamlit/pages/4_Siłownia.py
@@ -0,0 +1,12 @@
+# gym
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="Siłownia", 
+    page_icon="🏋️‍♀️", 
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+st.markdown("# Treningi na siłowni")
