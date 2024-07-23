c1aece21341bcb9dfec178f83b5d8f085ba53dda
przemekolender <olenderprzemek@gmail.com>
2024-07-23 17:03:55 +0200

    Add sidebar to running tab

--- streamlit/pages/2_Bieganie.py
+++ streamlit/pages/2_Bieganie.py
@@ -1,6 +1,12 @@
 # running
 
 import streamlit as st
+import altair as alt
+import datetime
+import pandas as pd
+from data_processing import filter_by_period
+import plotly.express as px
+from palletes import *
 
 st.set_page_config(
     page_title="Bieganie", 
@@ -10,6 +16,63 @@ st.set_page_config(
 )
 
 st.markdown("# Bieganie")
+
+alt.themes.enable("dark")
+
+if "workouts" not in st.session_state:
+    st.session_state["workouts"] = pd.read_csv("files/workouts.csv")
+
+if "calendar" not in st.session_state:
+    st.session_state["calendar"] = pd.read_csv("files/calendar.csv")
+
+if "min_date" not in st.session_state:
+    st.session_state["min_date"] = st.session_state["calendar"]['date'].min()
+
+if "max_date" not in st.session_state:
+    st.session_state["max_date"] = datetime.datetime.today().strftime(format='%Y-%m-%d')
+
+
+###############################################################################################
+# sidebar options
+###############################################################################################
+with st.sidebar:   
+    year_list = list(st.session_state["calendar"]['year'].unique())[::-1]
+    selected_year = st.selectbox('Wybierz rok', ['-'] + year_list, index=len(year_list)-1)
+
+    month_list = list(st.session_state["calendar"][st.session_state["calendar"]['year'] == selected_year]['month_name_pl'].unique())
+    selected_month = st.selectbox('Wybierz miesiąc', ['-'] + month_list)
+
+    if selected_year == '-' and selected_month == '-':
+        st.session_state["min_date"] = st.session_state["calendar"]['date'].min()
+        st.session_state["max_date"] = st.session_state["calendar"]['date'].max()
+
+    elif selected_year != '-' and selected_month == '-':
+        st.session_state["min_date"] = f"{selected_year}-01-01"
+        st.session_state["max_date"] = f"{selected_year}-12-31"
+
+    else:
+        selected_month_num = st.session_state["calendar"][st.session_state["calendar"]["month_name_pl"] ==  selected_month]['month_str'].unique()[0]
+        min_day = '01'
+        max_day = st.session_state["calendar"][st.session_state["calendar"]["month_name_pl"] ==  selected_month]['day_num'].unique()[0]
+
+        st.session_state["min_date"] = f"{selected_year}-{selected_month_num}-{min_day}"
+        st.session_state["max_date"] = f"{selected_year}-{selected_month_num}-{max_day}"
+
+
+    granulation_name = st.radio(
+        label="Wybierz granulację",
+        options=['Miesiąc', 'Tydzień', 'Dzień']
+    )
+    granultaion_translation = {'Miesiąc' : 'month', 'Tydzień' : 'week', 'Dzień' : 'day_of_year'}
+    granulation = granultaion_translation[granulation_name]
+
+
+workouts = filter_by_period(
+    st.session_state["workouts"],
+    'date',
+    st.session_state['min_date'],
+    st.session_state['max_date']
+)
 
 
 
