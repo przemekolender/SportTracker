ea4e129c826e0fa5acf9e1cc03f2426e74aa8470
przemekolender <olenderprzemek@gmail.com>
2024-07-23 14:01:02 +0200

    Add folder where data is stored

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ def main():
 
 if __name__ == "__main__":
     for file in os.listdir("files"):
-        os.remove(f"files/{file}")
+        if '.csv' in file:
+            os.remove(f"files/{file}")
 
     w = Workouts()
     w.workouts.to_csv("files/workouts.csv")
