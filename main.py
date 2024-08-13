d5508be19ac4dc2f2c19c9788c2b48ad9b0277ad
przemekolender <olenderprzemek@gmail.com>
2024-08-13 20:45:49 +0200

    Add powershell script to update files, update main

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+from data.workouts import Workouts
+from data.data_processing import load_calendar, get_data
 import os
 
 
 def main():
-    os.system("streamlit run streamlit/app.py")
+    for file in os.listdir("files"):
+        if '.csv' in file:
+            os.remove(f"files/{file}")
+
+    w = Workouts()
+    w.workouts.to_csv("files/workouts.csv")
+    
+    calendar = load_calendar()
+    calendar.to_csv("files/calendar.csv")
+
+    #os.system("streamlit run streamlit/app.py")
 
 
 if __name__ == "__main__":  
