350151589fc98dc352980fd0f959217e094cfc73
przemekolender <olenderprzemek@gmail.com>
2024-12-27 01:04:42 +0100

    Rework on main.ps1, fix git errors

--- main.ps1
+++ main.ps1
@@ -1,27 +1,36 @@
-& {
-    Write-Output "Switching to correct branch...`n"
-    git checkout main
-    Write-Output "`nFinished switching to correct branch`n`n"
+$ErrorActionPreference="Stop"
 
-    Write-Output "Pulling code...`n"
-    git pull origin main
-    Write-Output "`nFinished pulling code`n`n"
+Remove-Item log.txt
+New-Item log.txt
 
-    Write-Output "Activating enviorement...`n"
-    .\sporttracker_env\Scripts\Activate.ps1
-    Write-Output "`nFinished activating enviorement`n`n"
+try{
+    & {
+        Write-Output "Switching to correct branch $(Get-Date -Format "dd.MM.yyyy HH:mm:ss")`n"
+        git checkout main
+    
+        Write-Output "Pulling code $(Get-Date -Format "dd.MM.yyyy HH:mm:ss")`n"
+        git pull origin main
+    
+        Write-Output "Activating enviorement $(Get-Date -Format "dd.MM.yyyy HH:mm:ss")`n"
+        .\sporttracker_env\Scripts\Activate.ps1
+    
+        Write-Output "Running python code $(Get-Date -Format "dd.MM.yyyy HH:mm:ss")`n"
+        python -u main.py 
+        if ($LASTEXITCODE -ne 0) { throw "There was an error in Python code`n$out" }
+    
+        Write-Output "Adding new files to git $(Get-Date -Format "dd.MM.yyyy HH:mm:ss")`n"
+        #git add files
+    
+        Write-Output "Commit and push to GitHub $(Get-Date -Format "dd.MM.yyyy HH:mm:ss")`n"
+        $date = Get-Date -Format "dd.MM.yyyy HH:mm"
+        #git commit -m "Update files, $date"
+        #git push origin main
+    
+        Write-Output "Script finished with success $(Get-Date -Format "dd.MM.yyyy HH:mm:ss")`n"
+    
+     } | Tee-Object -FilePath log.txt
+}
+catch {
+    $_ | Tee-Object -FilePath log.txt -Append
+}
 
-    Write-Output "Running python code...`n"
-    python main.py
-    Write-Output "`nFinished running python code`n`n"
-
-    Write-Output "Adding new files to git...`n"
-    git add files
-    Write-Output "`nFinished adding new files to git`n`n"
-
-    Write-Output "Commit and push to GitHub...`n"
-    $date = Get-Date -Format "dd/MM/yyyy HH:mm"
-    git commit -m "Update files, $date"
-    git push origin main
-    Write-Output "`nFinished Commit and push to GitHub`n`n"
-} 2>&1 | Tee-Object -FilePath log.txt
