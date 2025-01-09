9725e93b7dbed202cba6a3f28f3452595cc445e9
przemekolender <olenderprzemek@gmail.com>
2025-01-09 23:48:24 +0100

    Add workouts from all years, refactor

--- streamlit/pages/6_Ostatnie treningi.py
+++ streamlit/pages/6_Ostatnie treningi.py
@@ -17,7 +17,7 @@ st.markdown("# Ostatnie treningi")
 alt.theme.enable("dark")
 
 if "workouts" not in st.session_state:
-    st.session_state["workouts"] = pd.read_csv("files/workouts.csv", sep='|')
+    st.session_state["workouts"] = pd.read_csv("files/workouts.csv", sep='|').fillna('')
 
 if "counter" not in st.session_state:
     st.session_state["counter"] = 0
@@ -52,28 +52,41 @@ with col3:
 ###############################################################################################
 # preparing data & drawing table
 ###############################################################################################
-chosen_columns = ['index', 'date', 'sport', 'exercise', 'details', 'comments']
+chosen_columns = ['date', 'sport', 'exercise', 'details', 'comments']
 
 if multiselect is not None:
     # prepare dataframe to show
-    w = st.session_state["workouts"].loc[st.session_state["workouts"]['sport'] == multiselect, chosen_columns]
-    w['index2'] = list(range(w.shape[0]))
-    w['comments'] = w['comments'].fillna('')
-    w = w.groupby(chosen_columns).agg({'index2' : 'min'}).sort_values('index2').reset_index()[chosen_columns]
+
+    if multiselect == 'bieganie':
+        w = st.session_state["workouts"].loc[st.session_state["workouts"]['sport'] == multiselect, chosen_columns] \
+            .sort_values('date', ascending=True) \
+            .reset_index(drop = True)
+        
+    else:
+        w = st.session_state["workouts"].loc[st.session_state["workouts"]['sport'] == multiselect, chosen_columns] \
+            .reset_index() \
+            .groupby(chosen_columns).agg({'index' : 'max'}).reset_index() \
+            .sort_values('index', ascending=True) \
+            .reset_index(drop = True)
+            
     
-    # fix indexes to iterate easly on the dataframe
-    d = {}
-    u = w['index'].unique().tolist()
-    for i in range(len(u)):
-        d[int(u[i])] = i
-    w['index'] = w['index'].apply(lambda x : d[x])
+    j = -1
+    prev = w.loc[0, 'date']
+    for i in range(len(w)):
+        curr = w.loc[i, 'date']
+        if prev != curr:
+            j -= 1
+        w.loc[i, 'index2'] = j
+        prev = curr
+
+    w['index2'] = w['index2'].astype(int)
 
     # make sure you cannot go out of range of activities
     st.session_state['counter'] = min(0, st.session_state['counter'])
-    st.session_state['counter'] = max(-(len(u)-1), st.session_state['counter'])
+    st.session_state['counter'] = max(w['index2'].min()+1, st.session_state['counter'])
     
     #print table
-    df_print = w.loc[w['index'] == (len(u)-1 + st.session_state['counter']), ['date', 'exercise', 'details', 'comments']].reset_index(drop = True)
+    df_print = w.loc[w['index2'] == (w['index2'].min() - st.session_state['counter']), ['date', 'exercise', 'details', 'comments']].reset_index(drop = True)
     df_print.columns = ['Data', 'Ćwiczenie', 'Szczegóły', 'Komentarze']
     st.table(df_print)
-    st.markdown(f"Trening {len(u) + st.session_state['counter']} / {len(u)}")
+    st.markdown(f"Trening {-1 * w['index2'].min() + st.session_state['counter']} / {-1 * w['index2'].min()}")
