333a2c7e8b7ca2189324b2c0de3a7775c4f57d77
przemekolender <olenderprzemek@gmail.com>
2025-03-09 21:33:33 +0100

    Change session variable names

--- streamlit/pages/6_Ostatnie treningi.py
+++ streamlit/pages/6_Ostatnie treningi.py
@@ -18,18 +18,19 @@ st.markdown("# Ostatnie treningi")
 
 alt.theme.enable("dark")
 
-if "workouts" not in st.session_state:
-    st.session_state["workouts"] = pd.read_csv("files/workouts.csv", sep='|').fillna('')
+if "workouts_lw" not in st.session_state:
+    st.session_state["workouts_lw"] = pd.read_csv("files/workouts.csv", sep='|').fillna('')
+    st.session_state["workouts_lw"]["date"] = pd.to_datetime(st.session_state["workouts_lw"]["date"], format='%Y-%m-%d')
 
-if "calendar" not in st.session_state:
-    st.session_state["calendar"] = pd.read_csv("files/calendar.csv", sep='|')
-    st.session_state["calendar"]["date"] = pd.to_datetime(st.session_state["calendar"]["date"], format='%Y-%m-%d')
+if "calendar_lw" not in st.session_state:
+    st.session_state["calendar_lw"] = pd.read_csv("files/calendar.csv", sep='|')
+    st.session_state["calendar_lw"]["date"] = pd.to_datetime(st.session_state["calendar_lw"]["date"], format='%Y-%m-%d')
 
-if "min_date" not in st.session_state:
-    st.session_state["min_date"] = st.session_state["calendar"]['date'].min()
+if "min_date_lw" not in st.session_state:
+    st.session_state["min_date_lw"] = st.session_state["calendar_lw"]['date'].min()
 
-if "max_date" not in st.session_state:
-    st.session_state["max_date"] = datetime.datetime.today().strftime(format='%Y-%m-%d')
+if "max_date_lw" not in st.session_state:
+    st.session_state["max_date_lw"] = datetime.datetime.today().strftime(format='%Y-%m-%d')
 
 if "counter" not in st.session_state:
     st.session_state["counter"] = 0
@@ -39,9 +40,9 @@ if "counter" not in st.session_state:
 # sidebar options
 ###############################################################################################
 # always use dates no greater than today
-st.session_state["calendar"] = filter_by_period(
-    st.session_state["calendar"],
-    'date', st.session_state["calendar"]['date'].min(),
+st.session_state["calendar_lw"] = filter_by_period(
+    st.session_state["calendar_lw"],
+    'date', st.session_state["calendar_lw"]['date'].min(),
     datetime.datetime.today().strftime(format='%Y-%m-%d')
 )
 
@@ -50,12 +51,12 @@ with st.sidebar:
     # select start year and month
     col_year_start, col_month_start = st.columns(2)
     with col_year_start:
-        year_list_start = st.session_state["calendar"]['year'].unique().tolist()
+        year_list_start = st.session_state["calendar_lw"]['year'].unique().tolist()
         selected_year_start = st.selectbox('Rok początkowy', year_list_start, index=0)
   
     with col_month_start:
-        month_list_start = st.session_state["calendar"].loc[
-            st.session_state["calendar"]['year'] == selected_year_start, ['month','month_name_pl']
+        month_list_start = st.session_state["calendar_lw"].loc[
+            st.session_state["calendar_lw"]['year'] == selected_year_start, ['month','month_name_pl']
         ].groupby(['month','month_name_pl']).all().reset_index()
         selected_month_start = st.selectbox('Miesiąc początkowy', month_list_start['month_name_pl'].tolist(), index = 0)
         selected_month_start_int = month_list_start.loc[month_list_start['month_name_pl'] == selected_month_start, 'month'].tolist()[0]
@@ -64,28 +65,28 @@ with st.sidebar:
     # select end year and month
     col_year_end, col_month_end = st.columns(2)
     with col_year_end:
-        year_list_end = st.session_state["calendar"].loc[st.session_state["calendar"]['year'] >= selected_year_start, 'year'].unique().tolist()
+        year_list_end = st.session_state["calendar_lw"].loc[st.session_state["calendar_lw"]['year'] >= selected_year_start, 'year'].unique().tolist()
         selected_year_end = st.selectbox('Rok końcowy', year_list_end, index=len(year_list_end)-1)
 
     with col_month_end:
         if selected_year_end == selected_year_start:
-            month_list_end = st.session_state["calendar"].loc[
-                (st.session_state["calendar"]['year'] == selected_year_end) & 
-                (st.session_state["calendar"]['month'] >= selected_month_start_int)
+            month_list_end = st.session_state["calendar_lw"].loc[
+                (st.session_state["calendar_lw"]['year'] == selected_year_end) & 
+                (st.session_state["calendar_lw"]['month'] >= selected_month_start_int)
                 , 'month_name_pl'].unique().tolist()
         else:
-            month_list_end = st.session_state["calendar"].loc[(st.session_state["calendar"]['year'] == selected_year_end), 'month_name_pl'].unique().tolist()
+            month_list_end = st.session_state["calendar_lw"].loc[(st.session_state["calendar_lw"]['year'] == selected_year_end), 'month_name_pl'].unique().tolist()
         selected_month_end = st.selectbox('Miesiąc końcowy', month_list_end, index=len(month_list_end)-1)
 
 
     # set min_date and max_date according to selected values
-    selected_month_start_num = st.session_state["calendar"][st.session_state["calendar"]["month_name_pl"] ==  selected_month_start]['month_str'].unique()[0]
-    selected_month_end_num = st.session_state["calendar"][st.session_state["calendar"]["month_name_pl"] ==  selected_month_end]['month_str'].unique()[0]
+    selected_month_start_num = st.session_state["calendar_lw"][st.session_state["calendar_lw"]["month_name_pl"] ==  selected_month_start]['month_str'].unique()[0]
+    selected_month_end_num = st.session_state["calendar_lw"][st.session_state["calendar_lw"]["month_name_pl"] ==  selected_month_end]['month_str'].unique()[0]
     min_day = '01'
-    max_day = st.session_state["calendar"][st.session_state["calendar"]["month_name_pl"] ==  selected_month_end]['day_num'].unique()[0]
+    max_day = st.session_state["calendar_lw"][st.session_state["calendar_lw"]["month_name_pl"] ==  selected_month_end]['day_num'].unique()[0]
 
-    st.session_state["min_date"] = f"{selected_year_start}-{selected_month_start_num}-{min_day}"
-    st.session_state["max_date"] = f"{selected_year_end}-{selected_month_end_num}-{max_day}"
+    st.session_state["min_date_lw"] = f"{selected_year_start}-{selected_month_start_num}-{min_day}"
+    st.session_state["max_date_lw"] = f"{selected_year_end}-{selected_month_end_num}-{max_day}"
 
 
 ###############################################################################################
@@ -123,10 +124,10 @@ chosen_columns = ['date', 'sport', 'exercise', 'details', 'comments']
 if multiselect is not None:
     # prepare dataframe to show
     workouts = filter_by_period(
-        st.session_state["workouts"],
+        st.session_state["workouts_lw"],
         'date',
-        st.session_state['min_date'],
-        st.session_state['max_date']
+        st.session_state['min_date_lw'],
+        st.session_state['max_date_lw']
     )
 
     if multiselect == 'bieganie':
